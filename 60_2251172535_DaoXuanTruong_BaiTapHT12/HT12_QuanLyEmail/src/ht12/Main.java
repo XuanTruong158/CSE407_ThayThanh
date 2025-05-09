799c0f0e2a3ce65e247439f7fcbba5bef2799cbb
XuanTruong158 <152410123+XuanTruong158@users.noreply.github.com>
2025-05-10 04:15:22 +0700

    update code

---
+++ 60_2251172535_DaoXuanTruong_BaiTapHT12/HT12_QuanLyEmail/src/ht12/Main.java
@@ -0,0 +1,19 @@
+package ht12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        IEmailService ses = new SmtpEmailService();
+        EmailNotifier a = new EmailNotifier(ses);
+        a.Notify("user@example.com", "Chào bạn!");
+        a.Notify("", "Chào bạn");
+
+        IEmailService aes = new ApiEmailService();
+        EmailNotifier b = new EmailNotifier(aes);
+        b.Notify("", "Chào bạn!");
+        b.Notify("2251172535@gmail.com", "Chào bạn!");
+        b.Notify("       ", "Chào bạn!");
+	}
+
+}
